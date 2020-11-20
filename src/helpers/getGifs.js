28f9ad7c612de736629982a79bbe67add93390d4
Parsival Makto <parsival.turokmakto@gmail.com>
2020-11-20 14:50:51 -0600

    Actualizacion a HTTPS de la API

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async( categoria ) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI( categoria ) }&limit=10&api_key=FguaO7QFx7B6pJmqCFeZAru6JUZqlYhc`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( categoria ) }&limit=10&api_key=FguaO7QFx7B6pJmqCFeZAru6JUZqlYhc`;
     const resp = await fetch( url );
     const { data } = await resp.json();
     
